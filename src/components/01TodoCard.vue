e4a661b5458c2ea700d92fdbcde407ab159e73bc
IanTang <iantang@uitc.com.tw>
2024-07-23 15:21:43 +0800

    feat. 製作TodoCard組件

---
+++ src/components/01TodoCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card" style="width: 18rem">
+    <div class="card-header">{{ name }} / {{ title }}</div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item"></li>
+      <li class="list-group-item">A second item</li>
+      <li class="list-group-item">A third item</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps(['data']);
+
+const name = ref(props.data.name);
+const title = ref(props.data.title);
+</script>
+
+<style scoped></style>
--- src/views/pages/01PageView.vue
+++ src/views/pages/01PageView.vue
@@ -1,15 +1,46 @@
 <template>
   <h1>Hi</h1>
-  <p>{{ test }}</p>
+  <!-- <table>
+    <tr v-for="data in datas" :key="data.todoId">
+      <td>{{ data.name }}</td>
+      <td>{{ data.title }}</td>
+      <td>{{ data.todoContent }}</td>
+      <td>{{ data.isComplete }}</td>
+      <td>{{ data.addTime }}</td>
+      <td>{{ data.completeTime }}</td>
+    </tr>
+  </table> -->
+  <TodoCard v-for="data in datas" :key="data.todoId" :data="data"></TodoCard>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue';
 import axiosapi from '../../../plugins/axiosapi';
+import Swal from 'sweetalert2';
+import TodoCard from '@/components/01TodoCard.vue';
 
-const test = ref();
+Swal.fire({
+  text: 'Loading......',
+  showConfirmButton: false,
+  allowOutsideClick: false
+});
+
+const datas = ref();
+
 onMounted(async () => {
-  test.value = await axiosapi.get(`Get`);
+  try {
+    const response = await axiosapi.get(`Get`);
+    datas.value = [...response.data.returnData];
+    console.log(datas);
+    Swal.close();
+  } catch (err) {
+    Swal.fire({
+      text: `error: ${err.message}`,
+      icon: 'error',
+      allowOutsideClick: false,
+      confirmButtonText: '確認'
+    });
+  }
 });
 </script>
 
