2dd0d5e64edeb5f305a4f5c7f242cb9302501d5e
IanTang <iantang@uitc.com.tw>
2024-07-26 10:56:53 +0800

    feat. 新增todo
    axios加入攔截器

--- plugins/axiosapi.js
+++ plugins/axiosapi.js
@@ -1,5 +1,22 @@
 import axios from 'axios';
+import Swal from 'sweetalert2';
 
-export default axios.create({
+const axiosapi = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL
 });
+
+axiosapi.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    Swal.fire({
+      text: `error: ${error.message}`,
+      icon: 'error',
+      allowOutsideClick: false,
+      confirmButtonText: '確認'
+    });
+  }
+);
+
+export default axiosapi;
--- src/views/pages/01PageView.vue
+++ src/views/pages/01PageView.vue
@@ -1,17 +1,44 @@
 <template>
-  <h1>Todo List</h1>
-  <!-- <table>
-    <tr v-for="data in datas" :key="data.todoId">
-      <td>{{ data.name }}</td>
-      <td>{{ data.title }}</td>
-      <td>{{ data.todoContent }}</td>
-      <td>{{ data.isComplete }}</td>
-      <td>{{ data.addTime }}</td>
-      <td>{{ data.completeTime }}</td>
-    </tr>
-  </table> -->
+  <h1 style="margin-bottom: 10px">Todo List</h1>
   <div class="row">
-    <div class="col-5"></div>
+    <div class="col-5">
+      <form @submit="postTodo">
+        <div class="mb-3">
+          <h2>請填寫代辦事項</h2>
+          <label for="inputName" class="form-label">姓名</label>
+          <input
+            type="text"
+            class="form-control"
+            id="inputName"
+            name="name"
+            v-model="name"
+            required
+          />
+        </div>
+        <div class="mb-3">
+          <label for="inputTitle" class="form-label">代辦標題</label>
+          <input
+            type="text"
+            class="form-control"
+            id="inputTitle"
+            name="title"
+            v-model="title"
+            required
+          />
+        </div>
+        <div class="mb-3">
+          <label for="inputText" class="form-label" name="todoContent">代辦內容</label>
+          <textarea
+            class="form-control"
+            id="inputText"
+            style="height: 100px; resize: none"
+            v-model="todoContent"
+            required
+          ></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">儲存</button>
+      </form>
+    </div>
     <div class="col-7">
       <div class="justify-content-center">
         <TodoCard v-for="data in datas" :key="data.todoId" :data="data"></TodoCard>
@@ -34,19 +61,23 @@ Swal.fire({
 
 const datas = ref();
 
+const name = ref('');
+const title = ref('');
+const todoContent = ref('');
+
+const postTodo = async () => {
+  const req = {
+    name: name.value,
+    title: title.value,
+    todoContent: todoContent.value
+  };
+  await axiosapi.post('InsertTodo', req);
+};
+
 onMounted(async () => {
-  try {
-    const response = await axiosapi.get(`Get`);
-    datas.value = [...response.data.returnData];
-    Swal.close();
-  } catch (err) {
-    Swal.fire({
-      text: `error: ${err.message}`,
-      icon: 'error',
-      allowOutsideClick: false,
-      confirmButtonText: '確認'
-    });
-  }
+  const response = await axiosapi.get(`Get`);
+  datas.value = [...response.data.returnData];
+  Swal.close();
 });
 </script>
 
