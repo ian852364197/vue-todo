e50d45e6f433cb66dd7ac7beb8d4464cc3d534c9
IanTang <iantang@uitc.com.tw>
2024-07-26 14:32:55 +0800

    feat. 依完成狀態改變樣式

--- src/components/01TodoCard.vue
+++ src/components/01TodoCard.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="card" style="width: 80%; margin-bottom: 10px">
+  <div class="card" :class="cardClass" style="width: 80%; margin-bottom: 10px">
     <div class="card-header">
       <div class="d-flex justify-content-between">
-        <div>{{ name }} / {{ title }}</div>
+        <div>{{ cardTitle }}</div>
         <div>{{ completeTime }}</div>
       </div>
     </div>
@@ -16,15 +16,11 @@
         >
           完成
         </button>
-        <button
-          class="btn btn-outline-primary"
-          style="margin-left: 5px"
-          @click="emits('update', data)"
-        >
+        <button :class="buttonClass" style="margin-left: 5px" @click="emits('update', data)">
           編輯
         </button>
         <button
-          class="btn btn-outline-dark"
+          class="btn btn-outline-danger"
           style="margin-left: 5px"
           @click="emits('delete', data.todoId)"
         >
@@ -48,6 +44,10 @@ const title = ref(data.title);
 const content = ref(data.todoContent);
 //將Y、N計算為boolean
 const isComplete = computed(() => data.isComplete === 'Y');
+const cardTitle = computed(() => {
+  let icon = isComplete.value ? '✅' : 'ℹ';
+  return icon + ' ' + name.value + '/ ' + title.value;
+});
 //設定completeTime
 const completeTime = computed(() => {
   if (isComplete.value) {
@@ -58,6 +58,21 @@ const completeTime = computed(() => {
   }
   //未完成
   return '未完成';
+});
+//依完成狀態改變樣式
+const cardClass = computed(() => {
+  return {
+    'border-success': isComplete.value,
+    'border-primary': !isComplete.value,
+    'text-success': isComplete.value,
+    'text-primary': !isComplete.value
+  };
+});
+const buttonClass = computed(() => {
+  return {
+    'btn btn-outline-success': isComplete.value,
+    'btn btn-outline-primary': !isComplete.value
+  };
 });
 </script>
 
