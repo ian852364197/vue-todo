788515c0a3909d3382a3bb3d1a488d6e0cceb7a3
IanTang <iantang@uitc.com.tw>
2024-07-26 13:57:35 +0800

    feat. 更新攔截器

--- plugins/axiosapi.js
+++ plugins/axiosapi.js
@@ -6,14 +6,28 @@ const axiosapi = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL
 });
 
+//處理錯誤訊息
+const handleErrorCode = (resData) => {
+  Swal.fire({
+    text: `錯誤: ${resData.returnMessage}`,
+    icon: 'error',
+    allowOutsideClick: false,
+    confirmButtonText: '確認'
+  });
+};
+
 //攔截response的error
 axiosapi.interceptors.response.use(
   (response) => {
-    return response;
+    if (response.data.returnCode == '2000') {
+      return response;
+    }
+    //returnCode不是2000時處理錯誤訊息
+    handleErrorCode(response.data);
   },
   (error) => {
     Swal.fire({
-      text: `error: ${error.message}`,
+      text: `預期外的錯誤: ${error.message}`,
       icon: 'error',
       allowOutsideClick: false,
       confirmButtonText: '確認'
